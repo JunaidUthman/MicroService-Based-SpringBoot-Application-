d9784b71414ac62c6aedcd29e2dd4bf0b8beb9ed
Junaid <junaiduthman123@gmail.com>
2025-11-09 22:26:08 +0100

    remarque

--- customer/src/main/java/com/submodule/customer/CustomerConfig.java
+++ customer/src/main/java/com/submodule/customer/CustomerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerConfig {
 
 
-    //RestTemplate is a synchronous client-side object provided by Spring that is used for consuming RESTful web services.
+    //RestTemplate is a synchronous(for asynchronous,use kafka or rabbitMQ) client-side object provided by Spring that is used for consuming RESTful web services.
     //but its not the best way to connet two services , cause u can see in CustomerService that the service gotta know all the microservices
     //which is not good if there is a lot of services
     // thats why u gotta use a Discovery Service (like eureka), that knows all addresses of microservices.
